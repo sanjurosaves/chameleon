9fea3c342e70d5fe1eb23a3a03f45075c581284f
Alex Allen <325@holbertonschool.com>
2018-09-26 22:22:17 +0000

    update dpcm.h, get_diff.c

--- eoy_excercises/dpcm.h
+++ eoy_excercises/dpcm.h
@@ -3,18 +3,13 @@
 
 /**
  * struct stereo_sample - structure for 16-bit stereo sample
- * @hi: high-order (leftmost) byte of 16-bit sample
- * @lo: low-order (rightmost) byte of 16-bit sample
  * @l: value of left-channel sample
  * @r: value of right-channel sample
  * @pl: value of previous left-channel sample
  * @pr: value of previous right-channel sample
- * @shifted: high-order byte expanded to 16-bit value and shifted left 8 bits
  */
 struct stereo_sample {
-	char hi, lo;
 	int l, r, pl, pr;
-	short int shifted;
 };
 
 typedef struct stereo_sample sample;
